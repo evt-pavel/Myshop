b3dfbfa250c4b12c60e14d2acb2c6a7a3d5de052
pwl-ev <pwl93.e@gmail.com>
2023-07-11 23:38:01 +0300

    add templates for basket

--- cart/cart.py
+++ cart/cart.py
@@ -12,12 +12,12 @@ class Cart:
 
         self.cart = cart
 
-    def add(self, product, quantity=1, override_quntity=False):
+    def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 0,
                                      'price': str(product.price)}
-        if override_quntity:
+        if override_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
@@ -40,14 +40,14 @@ class Cart:
             cart[str(product.id)]['product'] = product
         for item in cart.values():
             item['price'] = Decimal(item['price'])
-            item['total_prrice'] = item['price'] * item['quatity']
+            item['total_price'] = item['price'] * item['quantity']
             yield item
 
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
     
     def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quntity'] 
+        return sum(Decimal(item['price']) * item['quantity'] 
                    for item in self.cart.values())
     
     def clear(self):
--- cart/views.py
+++ cart/views.py
@@ -12,7 +12,7 @@ def cart_add(request, product_id):
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product,
-                 qiantity=cd['quantity'],
+                 quantity=cd['quantity'],
                  override_quantity=cd['override'])
     return redirect('cart:cart_detail')
 
