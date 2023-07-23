ebfef4e9481a894750c0cba0ce569a57988df4c8
pwl-ev <pwl93.e@gmail.com>
2023-07-23 23:46:23 +0300

    add form and view for create order

--- orders/admin.py
+++ orders/admin.py
@@ -3,15 +3,15 @@ from .models import Order, OrderItem
 
 # Register your models here.
 class OrderItemInline(admin.TabularInline):
-    models = OrderItem
+    model = OrderItem
     raw_id_fields = ['product']
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id', 'firsr_name', 'last_name', 'email',
+    list_display = ['id', 'first_name', 'last_name', 'email',
                     'adress', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
-    list_filter = ['paid', 'created', 'updated']
+                    'created', 'update']
+    list_filter = ['paid', 'created', 'update']
     inlines = [OrderItemInline]
 
 
